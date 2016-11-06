4ee1095bf5bd7e9b9185fe5138addaa20f58758a
hanifekurnaz <hanife.kurnaz@bil.omu.edu.tr>
2016-11-06 11:27:14 +0200

    Update pingsweep.sh

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-for i in {1..254};
+sayac=0
+ipadd=$(ifconfig | grep "broadcast" | cut -d " " -f 10 | cut -d "." -f 1,2,3)
+for i in {1..3};
 do
-    ping $(ifconfig | grep "broadcast" | cut -d " " -f 10 | cut -d "." -f 1,2,3).$i
+    ping $ipadd.$i
+    if [ $(ping -n 1 $ipadd.$i | grep icmp* | wc -l) -gt 0 ]
+    then
+        let sayac+=1
+    fi
 done
+if [ $sayac -eq 0 ]
+then
+    echo "Aktif host yoktur"
+else
+    echo "Aktif bilgisayar sayısı : " $sayac
+fi
